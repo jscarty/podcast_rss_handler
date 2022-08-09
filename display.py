c695ed6da2c016778917cf0df1db30de34cc5098
jscarty <K12147284@students.jku.at>
2022-08-09 22:23:12 +0200

    adding scrollbar

--- display.py
+++ display.py
@@ -3,6 +3,7 @@ import feedparser
 from tkinter import *
 from tkinter import ttk
 
+
 class Feed:
 
     def __init__(self, root):
@@ -14,15 +15,19 @@ class Feed:
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
         self.title_list = Listbox(mainframe, height=20, width=40)
+        self.title_list.bind('<<ListboxSelect>>', self.show_episode_description)
+        self.status_message = StringVar()
+        self.scrollbar = ttk.Scrollbar(mainframe, orient=VERTICAL, command=self.title_list.yview())
+        self.status = ttk.Label(mainframe, textvariable=self.status_message, anchor=S).grid(row=6, column=1, columnspan=4)
         self.feed_location = StringVar()
         feed_entry = ttk.Entry(mainframe, width=30, textvariable=self.feed_location)
         feed_entry.grid(column=3, row=1, sticky=(W, E))
         ttk.Label(mainframe, text="Feed").grid(column=2, row=1, sticky=(W, E))
 
 
-        ttk.Button(mainframe, text="Get Feed", command=self.get_feed_from_url).grid(column=5, row=1, sticky=E)
+        ttk.Button(mainframe, text="Get Feed", command=self.get_feed_from_url).grid(column=6, row=1, sticky=E)
 
-        ttk.Label(mainframe, text="Titles").grid(column=2, row=3, sticky=N)
+        ttk.Label(mainframe, text="Titles").grid(column=2, row=3, sticky=(W, E))
         ttk.Button(mainframe, text="Get Titles", command=self.print_episode_titles).grid(column=3, row=3, sticky=W)
 
         feed_entry.focus()
@@ -49,20 +54,37 @@ class Feed:
 
         titles = []
         print(type(self.feed_object))
-        print("trying")
-        episode_number = len(self.feed_object.entries)
-        for episode in self.feed_object.entries:
+        for index, episode in enumerate(self.feed_object.entries):
 
-            print(episode.title)
+            # print(f"episode number is {episode_number} and title is {episode.title}")
+
             titles.append(episode.title)
-            self.title_list.insert(episode_number, episode.title)
-            episode_number -= 1
-            self.title_list.grid(column=0, row=4, sticky=S)
+            self.title_list.insert(index, episode.title)
+
+            self.title_list.grid(column=1, row=4, columnspan=3, rowspan=2)
+
+
             # self.title_list.pack()
             # yield episode.title
+        self.scrollbar.grid(column=5, row=4, sticky=(N, S))
+        # self.title_list['yscrollcommand'] =
+        self.title_list.configure(yscrollcommand= self.scrollbar.set)
+        root.grid_columnconfigure(0, weight=1)
+        root.grid_rowconfigure(0, weight=1)
         # titles_string = StringVar(value=titles)
         # Listbox  = Listbox(ma)
         # ttk.Label(mainframe, textvariable=self.)
+    def show_episode_description(self, *args):
+
+        indexs = self.title_list.curselection()
+        if len(indexs) ==1:
+
+            index = int(indexs[0])
+
+            episode = self.feed_object.entries[index]
+            self.status_message.set(episode.link)
+
+
 
 
 root = Tk()
