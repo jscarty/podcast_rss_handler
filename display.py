97d50ef681fea0f29f09dbd55ae769a9ea11b99c
jscarty <K12147284@students.jku.at>
2022-08-30 23:51:12 +0200

    scrollbar works and html description of episode.

--- display.py
+++ display.py
@@ -2,7 +2,8 @@ import os
 import feedparser
 from tkinter import *
 from tkinter import ttk
-
+from tkinterhtml import HtmlFrame
+from tkhtmlview import HTMLLabel
 
 class Feed:
 
@@ -17,13 +18,16 @@ class Feed:
         self.title_list = Listbox(mainframe, height=20, width=40)
         self.title_list.bind('<<ListboxSelect>>', self.show_episode_description)
         self.status_message = StringVar()
-        self.scrollbar = ttk.Scrollbar(mainframe, orient=VERTICAL, command=self.title_list.yview())
-        self.status = ttk.Label(mainframe, textvariable=self.status_message, anchor=S).grid(row=6, column=1, columnspan=4)
+        self.scrollbar = ttk.Scrollbar(mainframe, orient=VERTICAL, command=self.title_list.yview)
+        # self.status = ttk.Label(mainframe, textvariable=self.status_message, anchor=W).grid(row=3, column=6, columnspan=4)
+        self.status = HTMLLabel(mainframe)
+        self.status.grid(row=3, column=6, columnspan=4)
         self.feed_location = StringVar()
         feed_entry = ttk.Entry(mainframe, width=30, textvariable=self.feed_location)
         feed_entry.grid(column=3, row=1, sticky=(W, E))
         ttk.Label(mainframe, text="Feed").grid(column=2, row=1, sticky=(W, E))
-
+        description = StringVar()
+        self.description_message = ttk.Label(mainframe, textvariable=description, anchor='w')
 
         ttk.Button(mainframe, text="Get Feed", command=self.get_feed_from_url).grid(column=6, row=1, sticky=E)
 
@@ -36,11 +40,9 @@ class Feed:
         try:
 
             feed_url = self.feed_location.get()
-            print(feed_url)
             feed = feedparser.parse(feed_url)
             self.feed_object = feed
-            print("Got feed")
-            print(type(self.feed_object))
+
             return feed
         except ValueError: "No URL provided"
         pass
@@ -57,8 +59,6 @@ class Feed:
         for index, episode in enumerate(self.feed_object.entries):
 
             # print(f"episode number is {episode_number} and title is {episode.title}")
-
-            titles.append(episode.title)
             self.title_list.insert(index, episode.title)
 
             self.title_list.grid(column=1, row=4, columnspan=3, rowspan=2)
@@ -67,10 +67,11 @@ class Feed:
             # self.title_list.pack()
             # yield episode.title
         self.scrollbar.grid(column=5, row=4, sticky=(N, S))
-        # self.title_list['yscrollcommand'] =
-        self.title_list.configure(yscrollcommand= self.scrollbar.set)
+        self.title_list['yscrollcommand'] = self.scrollbar.set
+        # self.title_list.configure(yscrollcommand= self.scrollbar.set)
         root.grid_columnconfigure(0, weight=1)
         root.grid_rowconfigure(0, weight=1)
+        self.title_list.bind('<<>ListboxSelect>', self.show_episode_description())
         # titles_string = StringVar(value=titles)
         # Listbox  = Listbox(ma)
         # ttk.Label(mainframe, textvariable=self.)
@@ -82,9 +83,18 @@ class Feed:
             index = int(indexs[0])
 
             episode = self.feed_object.entries[index]
-            self.status_message.set(episode.link)
 
+            self.status.set_html(episode.description)
+            """
+            At the moment tkhtmlview needed to be modified so the change in this github issue is added
+            https://github.com/bauripalash/tkhtmlview/pull/17
+            
+            Hopefully it will be updated on pypi
+            """
 
+    # def display_description(self, *args):
+    #     print('working')
+    #     description = StringVar()
 
 
 root = Tk()
